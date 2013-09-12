5b892b932ff56d7123db567f0946ea71856670b7
Lauri <git@lap.fi>
2013-09-12 18:55:51 +0300

    Declare tests order-dependent so that the order of examples in documentation matches the order of tests in code

--- lib/minitest/minitest_ext.rb
+++ lib/minitest/minitest_ext.rb
@@ -32,5 +32,13 @@ module MiniTest
 end
 
 module Kernel
-  alias_method :document, :describe
+  # `document` is an alias for `describe` but it also declares the tests
+  # order-dependent. This is because we would like to have the documentation
+  # always in the same order.
+  def document(desc, additional_desc=nil, &block)
+    describe desc, additional_desc do
+      i_suck_and_my_tests_are_order_dependent!
+      self.instance_eval(&block)
+    end
+  end
 end
